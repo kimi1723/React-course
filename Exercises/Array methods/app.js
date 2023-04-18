5b152ff2eb04e1809a57648a028a609e2b069c45
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-04-18 23:32:14 +0200

    Initial commit

---
+++ Exercises/Array methods/app.js
@@ -0,0 +1,14 @@
+function transformToObjects(numberArray) {
+	// Todo: Add your logic
+	// should return an array of objects
+
+	const arrayOfObjects = numberArray.map(num => {
+		return {
+			val: num,
+		};
+	});
+
+	console.log(arrayOfObjects);
+}
+
+transformToObjects([1, 2, 3]);
