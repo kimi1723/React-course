d239b2662723788832e23c22f5c0d574ea273fd9
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-06-25 12:09:28 +0200

    s

--- 1/01-starting-setup/src/components/Expenses/ExpenseFilter.js
+++ 1/01-starting-setup/src/components/Expenses/ExpenseFilter.js
@@ -6,13 +6,14 @@ const ExpenseFilter = (props) => {
     const yearPickedHandler = (e) => {
         const yearPicker = e.target;
         props.onYearChange(yearPicker.options[yearPicker.selectedIndex].value);
+        // props.onYearChange(yearPicker.value);
     }
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={yearPickedHandler}>
+        <select value={props.pickedYear} onChange={yearPickedHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
--- 1/01-starting-setup/src/components/Expenses/Expenses.js
+++ 1/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,21 +1,24 @@
+import {useState} from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 
 const Expenses = props => {
+	const [filteredYear, setFilteredYear] = useState('2019');
+
+	const yearChangeHandler = (pickedYear) => {
+		setFilteredYear(pickedYear);
+	}
+
 	const expenses = props.expenses.map(expense => {
 		return (
 			<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
 		);
 	});
 
-	const yearChangeHandler = (pickedYear) => {
-		console.log(pickedYear);
-	}
-
 	return (<div>
-		<ExpenseFilter onYearChange={yearChangeHandler}/>
+		<ExpenseFilter pickedYear={filteredYear} onYearChange={yearChangeHandler}/>
 		<Card className="expenses">{expenses}</Card>
 	</div>);
 };
