ff06ad445acc672f959ddb0c24c060ef532c5087
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-07-03 12:10:40 +0200

    m

--- practice project 2/01-starting-project/src/components/reusable-blocks/Button.js
+++ practice project 2/01-starting-project/src/components/reusable-blocks/Button.js
@@ -1,7 +1,7 @@
 import styles from './Button.module.css';
 
 const Button = (props) => {
-    return <button className={styles.button} onClick={props.onClick}>
+    return <button className={styles.button} onClick={props.onClick} type={props.type || "button"}>
         {props.children}
     </button>
 }
